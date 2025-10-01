6a5d9f21ba9b8d0a3432224426b23f55d28d51fd
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-10-01 12:37:10 +0300

    feat(DailyForecastCard): finish the component's markup and define the expected prop types

--- src/components/DailyForecastCard.tsx
+++ src/components/DailyForecastCard.tsx
@@ -1,6 +1,27 @@
-function DailyForecastCard() {
-  return (
-    <div>DailyForecastCard</div>
-  )
+type DailyForecastCardProps = {
+	day: string
+	icon: string
+	// Note that the temperatures are returned from the api as numbers, so I don't know if the
+	// conversion will be handled by the reducers or by this component through internal utilities.
+	// I am keeping things simple for now and just using strings
+	maxTemperature: string
+	minTemperature: string
+}
+function DailyForecastCard({ day, icon, maxTemperature, minTemperature }: DailyForecastCardProps) {
+	return (
+		<div className='font-dm-sans border border-neutral-600 bg-neutral-800 rounded-12 flex flex-col gap-200 grow items-center p-200'>
+			<p>{day}</p>
+			<p>
+				<img
+					src={icon}
+					alt=''
+				/>
+			</p>
+			<div className='temperature-range flex justify-between self-stretch'>
+				<p>{maxTemperature}</p>
+				<p>{minTemperature}</p>
+			</div>
+		</div>
+	)
 }
 export default DailyForecastCard
