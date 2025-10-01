b9aa297bec7ba82a86eba5e66deb42fd886958d2
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-10-01 09:59:25 +0300

    refactor: write the padding classes in the App component in a tailwind-ish way

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<div className=' px-16 py-8'>
 			<Navbar />
-			<h1 className='text-white text-3xl pt-[var(--spacing-600)] pr-[var(--spacing-1400)] pb-[var(--spacing-800)] pl-[var(--spacing-1400)] text-center text-[3.25rem] font-bricolage'>
+			<h1 className='text-white text-3xl pt-600 pr-1400 pb-800 pl-1400 text-center text-[3.25rem] font-bricolage'>
 				How's the sky looking today?{' '}
 			</h1>
 		</div>
