d0bac48b13cea2ba75eec64441f5f8e0f8a4e017
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-10-01 10:06:13 +0300

    refactor(SearchBox): modify the padding classes to use tailwind theme variables in tailwind's way

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,7 +2,7 @@ import searchIcon from '../assets/images/icon-search.svg'
 function SearchBox() {
 	return (
 		<div className='flex gap-2 justify-center'>
-			<div className='flex gap-4 rounded-md bg-neutral-800 p-[var(--spacing-200)] w-[32.75rem]'>
+			<div className='flex gap-4 rounded-md bg-neutral-800 p-200 w-[32.75rem]'>
 				{/* search box */}
 				<img
 					src={searchIcon}
@@ -14,9 +14,7 @@ function SearchBox() {
 					placeholder='Search for a place...'
 				/>
 			</div>
-			<button className='text-white bg-blue-500 py-[var(--spacing-200)] px-[var(--spacing-300)] rounded-[var(--radius-12)] hover:bg-blue-700 cursor-pointer'>
-				Search
-			</button>
+			<button className='text-white bg-blue-500 py-200 px-300 rounded-12 hover:bg-blue-700 cursor-pointer'>Search</button>
 		</div>
 	)
 }
