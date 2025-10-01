293e12ffa67d4bcfc613cb4bd4dc25354377e7b3
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-10-01 13:59:58 +0300

    feat(HourlyForecast): add the preliminary markup

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -1,6 +1,16 @@
+function Header() {
+	return (
+		<div className='flex justify-between items-center pb-200'>
+			<h4 className='font-semibold text-xl'>HourlyForecast</h4>
+			{/* DaysDropdown.. to do */}
+		</div>
+	)
+}
 function HourlyForecast() {
-  return (
-    <div>HourlyForecast</div>
-  )
+	return (
+		<div className='grow bg-neutral-800 p-300 rounded-20 relative'>
+			<Header />
+		</div>
+	)
 }
 export default HourlyForecast
