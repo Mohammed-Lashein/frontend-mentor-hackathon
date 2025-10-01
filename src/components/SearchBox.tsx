5a8a6440c52f84cb53d745fff1b56d5277d2444a
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-10-01 10:12:49 +0300

    feat(SearchBox): wrap the input and button tags in a form element for better accessibility

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,20 +1,21 @@
-import searchIcon from '../assets/images/icon-search.svg'
+import searchIcon from '@/assets/images/icon-search.svg'
 function SearchBox() {
 	return (
 		<div className='flex gap-2 justify-center'>
-			<div className='flex gap-4 rounded-md bg-neutral-800 p-200 w-[32.75rem]'>
-				{/* search box */}
-				<img
-					src={searchIcon}
-					alt='search icon'
-				/>
-				<input
-					type='text'
-					className='text-white bg-neutral-800 w-full focus:outline-none'
-					placeholder='Search for a place...'
-				/>
-			</div>
-			<button className='text-white bg-blue-500 py-200 px-300 rounded-12 hover:bg-blue-700 cursor-pointer'>Search</button>
+			<form className='flex gap-4'>
+				<div className='flex gap-4 rounded-md bg-neutral-800 p-200 w-[32.75rem]'>
+					<img
+						src={searchIcon}
+						alt='search icon'
+					/>
+					<input
+						type='text'
+						className='text-white bg-neutral-800 w-full focus:outline-none'
+						placeholder='Search for a place...'
+					/>
+				</div>
+				<button className='text-white bg-blue-500 py-200 px-300 rounded-12 hover:bg-blue-700 cursor-pointer'>Search</button>
+			</form>
 		</div>
 	)
 }
