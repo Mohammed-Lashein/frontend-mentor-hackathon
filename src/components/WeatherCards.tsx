846f9a396369775d374886b07ee39668cea8fcb5
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-10-05 18:16:36 +0300

    feat(WeatherCards): get the state from the store instead of the hardcoded values

--- src/components/WeatherCards.tsx
+++ src/components/WeatherCards.tsx
@@ -1,24 +1,29 @@
+import { useAppSelector } from '../hooks'
 import WeatherCard from './WeatherCard'
 
 function WeatherCards() {
-	const dataForCards = [
-		{
-			label: 'Feels Like',
-			value: '20°',
-		},
-		{
-			label: 'Humidity',
-			value: '46%',
-		},
-		{
-			label: 'Wind',
-			value: '14 km/h',
-		},
-		{
-			label: 'Precipitation',
-			value: '0mm',
-		},
-	]
+	const data = useAppSelector((state) => {
+		const weatherData = state.weatherData
+		return [
+			{
+				label: 'Feels Like',
+				value: weatherData.current.temperature_2m + state.currently_selected_units.temperature,
+			},
+			{
+				label: 'Humidity',
+				value: weatherData.current.relative_humidity_2m + weatherData.current_units.relative_humidity_2m,
+			},
+			{
+				label: 'Wind',
+				value: weatherData.current.wind_speed_10m + state.currently_selected_units.wind_speed,
+			},
+			{
+				label: 'Precipitation',
+				value: weatherData.current.precipitation + state.currently_selected_units.precipitation,
+			},
+		]
+	})
+	const dataForCards = data
 	return (
 		<div className='grid grid-cols-2 sm:grid-cols-4 pt-400 gap-300'>
 			{dataForCards.map(({ label, value }) => (
